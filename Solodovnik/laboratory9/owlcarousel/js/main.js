839bda3e3a9245914211bbac061daca3593ac6b1
Alexandr <qazwsxua98@gmail.com>
2020-05-16 18:43:16 +0300

    Laboratory 9

--- Solodovnik/laboratory9/owlcarousel/js/main.js
+++ Solodovnik/laboratory9/owlcarousel/js/main.js
@@ -4,3 +4,12 @@ $(document).ready(function(){
       loop: true
   });
 });
+
+$(document).ready(function () {
+$('span.pq').each(function () {
+var quote=$(this).clone();
+quote.removeClass('pq');
+quote.addClass('pullquote');
+$(this).before(quote);
+}); //end each
+}); //end ready
